adc988fc966696b30e18bb415642eb0e7afb7162
Ogih001-Ogih Gabriel <heavensent460@gmail.com>
2016-10-07 12:27:06 +0200

    new console app

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EleLoop(3, "me"));
+        }
+        public static string EleLoop(int n, string arr1)
+        {
+            return arr1 + EleLoop(n - 1, arr1);
+        }
+    }
+}
